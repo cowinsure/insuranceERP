24dadf95b43b891e0f456a9f5724ae06c5b51f47
cowinsure <cowinsure@gmail.com>
2025-09-21 00:00:03 +0600

    Fix: crop ploting base url chnaged

--- components/dialogs/crops/CreatePlotDialog.tsx
+++ components/dialogs/crops/CreatePlotDialog.tsx
@@ -158,7 +158,7 @@ export function CreatePlotDialog({ open, onOpenChange, onPlotCreated }: CreatePl
       
 
       // Make the API call
-      const response = await fetch("http://127.0.0.1:8000/landmap/generate", {
+      const response = await fetch("https://cropploting.dev.insurecow.com/landmap/generate/landmap/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

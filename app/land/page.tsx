faf875c3a2e09c99dcdec013c531c461d431c46d
cowinsure <cowinsure@gmail.com>
2025-11-04 12:28:51 +0600

    updated

--- app/land/page.tsx
+++ app/land/page.tsx
@@ -338,7 +338,7 @@ function groupByPointType(points: LandPoint[]) {
                     >
                       <div className="w-20 h-20 rounded-md overflow-hidden bg-gray-100 flex-shrink-0">
                         <img
-                          src={'https://insuranceportal-backend.insurecow.com/'+plot.image || '/placeholder.svg'}
+                          src={'https://dev-backend.insurecow.com/'+plot.image || '/placeholder.svg'}
                           alt={plot.land_name}
                           className="w-auto object-fit"
                         />

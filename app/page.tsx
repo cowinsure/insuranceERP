fa106cba46fd7fafb64616b0b604eaeabacedda5
cowinsure <cowinsure@gmail.com>
2026-02-03 11:29:28 +0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const router = useRouter();
   
 
+
   useEffect(() => {
     AOS.init();
     "main layout" + auth?.accessToken + localStorage.getItem("accessToken");
